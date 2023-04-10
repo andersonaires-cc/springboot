780169a793be909b30d88873d24bad701e118a60
anderson.silva <anderson.silva@SESAPRN-77356>
2023-04-10 17:56:56 -0300

    Incomplete convite form

--- src/main/java/br/com/first/system/Controllers/ReuniaoController.java
+++ src/main/java/br/com/first/system/Controllers/ReuniaoController.java
@@ -1,5 +1,16 @@
 package br.com.first.system.Controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reuniao")
 public class ReuniaoController {
     
+    @GetMapping
+    public String form(){
+        return "reuniao/form";
+    }
 }
