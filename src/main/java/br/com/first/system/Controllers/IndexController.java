c606c84b77804b13d805d168e3a33b1943a16bdf
anderson.silva <anderson.silva@SESAPRN-77356>
2023-04-10 16:35:49 -0300

    Added Index Controller

---
+++ src/main/java/br/com/first/system/Controllers/IndexController.java
@@ -0,0 +1,16 @@
+package br.com.first.system.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping; //Anotação para mapear GETsolicitações HTTP em métodos de manipulador específicos.
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    
+    @GetMapping
+    public String index(){
+        return "index";
+    } 
+}
+
