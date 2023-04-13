2ec9126c77e9829e5f021b7b2d6905ab10b2aa39
Cassio Silveira <cassio.felipe.sarmento@gmail.com>
2023-04-13 12:39:13 -0300

    Added thymeleaf binding. Added validation.

--- src/main/java/br/com/first/system/Controllers/ReuniaoController.java
+++ src/main/java/br/com/first/system/Controllers/ReuniaoController.java
@@ -6,9 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.first.system.Dtos.ConviteDTO;
 import br.com.first.system.Repositories.ConvitesRepository;
@@ -23,7 +21,8 @@ public class ReuniaoController {
     final ConvitesRepository repository;
 
     @GetMapping("/form")
-    public String form(){
+    public String form(Model model){
+        model.addAttribute("convite", new ConviteDTO());
         return "reuniao/form";
     }
 
@@ -34,10 +33,11 @@ public class ReuniaoController {
     }
 
     @PostMapping
-    public String form(@Valid ConviteDTO convite, BindingResult br){
-
+    public String form(@Valid ConviteDTO convite, BindingResult br, Model model){
+        
         if( br.hasErrors() )
         {
+            model.addAttribute("convite", convite);
             return "redirect:/reuniao/form";
         }
 
