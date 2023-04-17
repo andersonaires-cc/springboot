ed347971845998a8bece32281a3fdb00a2f8664d
Cassio Silveira <cassio.felipe.sarmento@gmail.com>
2023-04-17 12:03:32 -0300

    M2 db configuration. Hibernate config. First entity Convite.

---
+++ src/main/java/br/com/first/system/Config/DatasourceConfig.java
@@ -0,0 +1,21 @@
+package br.com.first.system.Config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfig {
+    
+    @Bean
+    public DataSource getDataSourceH2(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.username("sa");
+        builder.password("sa");
+        builder.url("jdbc:h2:mem:testdb");
+        return builder.build();
+    }
+
+}
--- src/main/java/br/com/first/system/Models/Convite.java
+++ src/main/java/br/com/first/system/Models/Convite.java
@@ -1,10 +1,17 @@
 package br.com.first.system.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Convite {
+    
+    @Id @GeneratedValue
     int id = 0;
+    
     String nome;
     String email;
     String cpf;
