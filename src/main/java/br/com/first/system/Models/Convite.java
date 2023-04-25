276ca24d5074411500d6531193485f48fedb5937
anderson.silva <anderson.silva@SESAPRN-77356>
2023-04-25 12:48:09 -0300

    Configurations Database H2

--- src/main/java/br/com/first/system/Models/Convite.java
+++ src/main/java/br/com/first/system/Models/Convite.java
@@ -1,11 +1,18 @@
 package br.com.first.system.Models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Convite {
+
+    @Id @GeneratedValue
     int id = 0;
+
     String nome;
     String email;
     boolean vaiParticipar = false;
